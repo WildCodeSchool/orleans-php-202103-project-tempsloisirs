cb003accbf40e7c112d10d989b978a6fd759e2f8
Sylvain Blondeau <sylvain_blondeau@wilder.school>
2021-04-09 09:24:02 +0200

    Initial commit

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 15:38
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+abstract class AbstractController
+{
+    /**
+     * @var Environment
+     */
+    protected Environment $twig;
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(APP_VIEW_PATH);
+        $this->twig = new Environment(
+            $loader,
+            [
+                'cache' => !APP_DEV, // @phpstan-ignore-line
+                'debug' => APP_DEV,
+            ]
+        );
+        $this->twig->addExtension(new DebugExtension());
+    }
+}

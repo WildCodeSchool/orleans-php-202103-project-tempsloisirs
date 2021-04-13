760cc77275031f8882bedda2def891ecc95994eb
glochlainn <lochlainn.gadault@gmail.com>
2021-04-13 12:17:04 +0200

    rename branch us4 to S01US04_adminCA

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,25 +1,9 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: aurelwcs
- * Date: 08/04/19
- * Time: 18:40
- */
-
 namespace App\Controller;
 
 class AdminController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-
     public function board()
     {
         return $this->twig->render('Admin/board.html.twig');

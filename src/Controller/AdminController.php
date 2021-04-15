051237b130520c0593c5a8caed659e66217cbcc3
glochlainn <lochlainn.gadault@gmail.com>
2021-04-15 17:15:42 +0200

    new branch for S01US08

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,4 +16,21 @@ class AdminController extends AbstractController
 
         return $this->twig->render('Admin/board_index.html.twig', ['boardMembers' => $boardMembers]);
     }
+
+    public function addBoardMember()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $boardMember = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $adminManager = new AdminManager();
+            $adminManager->insert($boardMember);
+            header('Location:/admin/board/');
+        }
+
+        return $this->twig->render('Admin/board_add.html.twig');
+    }
 }
--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -4,5 +4,16 @@ namespace App\Model;
 
 class AdminManager extends AbstractManager
 {
-    public const TABLE = 'board';
+    public const BOARD = 'board';
+
+    public function insert(array $boardMember)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::BOARD . " (`image`, `firstname`, `surname`, `role`) VALUES (:image, :firstname, :surname, :role)");
+        $statement->bindValue('image', $boardMember['image'], \PDO::PARAM_STR);
+        $statement->bindValue('firstname', $boardMember['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue('surname', $boardMember['surname'], \PDO::PARAM_STR);
+        $statement->bindValue('role', $boardMember['role'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }

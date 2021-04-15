6f603d8c7d73bb8c38edac7b91e708d734c2fd65
eguesbaya <guesbaya@hotmail.fr>
2021-04-15 12:24:08 +0200

    Basic structure of the Admin-activities section

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+use App\Model\AdminManager;
+
+class AdminController extends AbstractController
+{
+    public function activities(): string
+    {
+        $adminManager = new AdminManager();
+        $activities = $adminManager->selectAll('name');
+        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
+    }
+
+        public function home(): string
+    {
+        return $this->twig->render('Admin/home.html.twig');
+    }
+
+
+
+    /* TODO
+    
+    public function delete(): string
+    {
+        $adminManager = new AdminManager();
+        $activities = $adminManager->selectAll('firstname');
+        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
+    }
+
+    public function addActivity(): string
+    {
+        $adminManager = new AdminManager();
+        $activities = $adminManager->selectAll('firstname');
+        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
+    }
+
+    public function editActivity(): string
+    {
+        $adminManager = new AdminManager();
+        $activities = $adminManager->selectAll('firstname');
+        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
+    }*/
+
+
+}
+

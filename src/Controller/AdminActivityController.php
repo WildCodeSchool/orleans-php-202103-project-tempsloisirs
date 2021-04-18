7dd5bbce67d8860e7e10df8477925ac8fa66efbc
eguesbaya <guesbaya@hotmail.fr>
2021-04-18 17:39:43 +0200

    Established routing

---
+++ src/Controller/AdminActivityController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\AdminActivityManager;
+
+class AdminActivityController extends AbstractController
+{
+    /**
+     * List items
+     */
+    public function addActivity(): string
+    {
+        $adminActivityManager = new AdminActivityManager();
+        $activities = $adminActivityManager->selectAll('name');
+
+        return $this->twig->render('Admin/Activity/add.html.twig', ['activities' => $activities]);
+    }
+
+    /**
+     * Show informations for a specific item
+     */
+    public function show(int $id): string
+    {
+        $itemManager = new ItemManager();
+        $item = $itemManager->selectOneById($id);
+
+        return $this->twig->render('Item/show.html.twig', ['item' => $item]);
+    }
+
+    /**
+     * Edit a specific item
+     */
+    public function edit(int $id): string
+    {
+        $itemManager = new ItemManager();
+        $item = $itemManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $item = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
+            $itemManager->update($item);
+            header('Location: /item/show/' . $id);
+        }
+
+        return $this->twig->render('Item/edit.html.twig', [
+            'item' => $item,
+        ]);
+    }
+
+    /**
+     * Add a new item
+     */
+    public function add(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $item = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $itemManager = new ItemManager();
+            $id = $itemManager->insert($item);
+            header('Location:/item/show/' . $id);
+        }
+
+        return $this->twig->render('Item/add.html.twig');
+    }
+
+    /**
+     * Delete a specific item
+     */
+    public function delete(int $id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $itemManager = new ItemManager();
+            $itemManager->delete($id);
+            header('Location:/item/index');
+        }
+    }
+}

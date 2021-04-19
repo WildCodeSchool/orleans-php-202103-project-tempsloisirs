66c0b62506ff6daa1a061872986cad53a2b980c5
eguesbaya <guesbaya@hotmail.fr>
2021-04-19 15:26:36 +0200

    Removed commented code. Deleted admin_activities.css. Removed stylesheets from stylesheet block.

--- src/Controller/AdminActivityController.php
+++ src/Controller/AdminActivityController.php
@@ -12,34 +12,4 @@ class AdminActivityController extends AbstractController
         $activities = $adminActivityManager->selectAll('name');
         return $this->twig->render('Admin/Activity/index.html.twig', ['activities' => $activities]);
     }
-
-
-
-    /* TODO
-    public function home(): string
-    {
-        return $this->twig->render('Admin/home.html.twig');
-    }
-
-    public function delete(): string
-    {
-        $adminManager = new AdminManager();
-        $activities = $adminManager->selectAll('firstname');
-        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
-    }
-
-    public function add(): string
-    {
-        $adminManager = new AdminActivityManager();
-        $activities = $adminManager->selectAll('name');
-        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
-    }
-
-
-    public function editActivity(): string
-    {
-        $adminManager = new AdminManager();
-        $activities = $adminManager->selectAll('firstname');
-        return $this->twig->render('Admin/activities.html.twig', ['activities'=> $activities]);
-    }*/
 }

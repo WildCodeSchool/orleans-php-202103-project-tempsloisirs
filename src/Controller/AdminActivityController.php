e45b3bb0d159d2d0172bb257e7799a8b16a30c0f
eguesbaya <guesbaya@hotmail.fr>
2021-04-21 23:36:03 +0200

    Routing and Controller working. Need to work on Edit view and edit pdo

--- src/Controller/AdminActivityController.php
+++ src/Controller/AdminActivityController.php
@@ -12,4 +12,25 @@ class AdminActivityController extends AbstractController
         $activities = $activityManager->selectAll('name');
         return $this->twig->render('Admin/Activity/index.html.twig', ['activities' => $activities]);
     }
+
+    public function edit(int $id): string
+    {
+        $activityManager = new ActivityManager();
+        $activities = $activityManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $activities = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
+            $activityManager->update($activity);
+            header('Location: /AdminActivity/show/' . $id);
+        }
+
+        return $this->twig->render('Admin/Activity/edit.html.twig', [
+            'activities' => $activities,
+        ]);
+    }
 }
--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -5,4 +5,16 @@ namespace App\Model;
 class ActivityManager extends AbstractManager
 {
     public const TABLE = 'activity';
+
+    /**
+     * Update item in database
+     */
+    public function update(array $activities): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name WHERE id=:id");
+        $statement->bindValue('id', $activities['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $activities['name'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }

3c84fd844729bf8130fcb3be086cbefe282d0559
eguesbaya <guesbaya@hotmail.fr>
2021-04-22 11:03:16 +0200

    Put all errors in one single table. Fixed description checks

--- src/Controller/AdminActivityController.php
+++ src/Controller/AdminActivityController.php
@@ -14,26 +14,22 @@ class AdminActivityController extends AbstractController
      */
     public function add(): string
     {
-        $errors = [];
-        $errorsLength = [];
+        $errorsEmpty = $errorsURL = $errorsTime = $errorsLength = $errors = [];
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
             $activities = array_map('trim', $_POST);
             $activities = array_map('ucfirst', $activities);
 
             // data checks
-            $errors = $this->validate($activities);
+            $errorsEmpty = $this->validateEmpty($activities);
             $errorsLength = $this->validateLength($activities);
+            $errorsURL = $this->validateURL($activities);
+            $errorsTime = $this->validateTime($activities);
+
+            $errors = array_merge($errorsEmpty, $errorsLength, $errorsURL, $errorsTime);
 
             if (empty($errors)) {
-                // if validation is ok, insert and redirection
-                $activityManager = new ActivityManager();
-                $activityManager->insert($activities);
-                header('Location:/adminActivity/index');
-            }
-
-            if (empty($errorsLength)) {
                 // if validation is ok, insert and redirection
                 $activityManager = new ActivityManager();
                 $activityManager->insert($activities);
@@ -42,44 +38,58 @@ class AdminActivityController extends AbstractController
         }
 
         return $this->twig->render('Admin/Activity/add.html.twig', [
-            'errors' => $errors, 'errorsLength' => $errorsLength]);
+            'errors' => $errors]);
     }
 
 /**
  * Data checks
  */
-    public function validate(array $activities): array
+    public function validateEmpty(array $activities): array
     {
-        $errors = [];
+        $errorsEmpty = [];
         if (empty($activities['name'])) {
-            $errors[] = "Le nom de l'activité est obligatoire.";
+            $errorsEmpty[] = "Le nom de l'activité est obligatoire.";
         }
 
         if (empty($activities['weekday'])) {
-            $errors[] = "Vous devez indiquer le jour.";
+            $errorsEmpty[] = "Vous devez indiquer le jour.";
         }
 
         if (empty($activities['instructor_name'])) {
-            $errors[] = "Le nom de l'animateur est obligatoire";
+            $errorsEmpty[] = "Le nom de l'animateur est obligatoire";
         }
 
         if (empty($activities['start_time'])) {
-            $errors[] = "Veuillez indiquer une heure de début pour cette activité";
+            $errorsEmpty[] = "Veuillez indiquer une heure de début pour cette activité";
         }
 
         if (empty($activities['end_time'])) {
-            $errors[] = "Veuillez indiquer une heure de début pour cette activité";
+            $errorsEmpty[] = "Veuillez indiquer une heure de début pour cette activité";
         }
 
-        if (filter_var($activities['image'], FILTER_VALIDATE_URL)) {
-            $errors[] = "Cette URL n'est pas valide.";
+        return $errorsEmpty;
+    }
+
+    public function validateURL(array $activities): array
+    {
+        $errorsURL = [];
+
+        if (!filter_var($activities['image'], FILTER_VALIDATE_URL)) {
+            $errorsURL[] = "Cette URL n'est pas valide.";
         }
+
+        return $errorsURL;
+    }
+
+    public function validateTime(array $activities): array
+    {
+        $errorsTime = [];
 
         if ($activities['start_time'] > $activities['end_time']) {
-            $errors[] = 'L\'heure de fin de l\'activité doit être postérieure à l\'heure de début.';
+            $errorsTime[] = 'L\'heure de fin de l\'activité doit être postérieure à l\'heure de début.';
         }
 
-        return $errors;
+        return $errorsTime;
     }
 
     public function validateLength(array $activities): array
@@ -87,33 +97,33 @@ class AdminActivityController extends AbstractController
         $errorsLength = [];
 
         if (strlen($activities['name']) > self::MAX_FIELD_LENGTH) {
-            $errorsLength[] = 'Le nom de l\'activité ne peut pas dépasser' . self::MAX_FIELD_LENGTH . ' caractères';
+            $errorsLength[] = 'Le nom de l\'activité ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
         }
 
         if (strlen($activities['instructor_name']) > self::MAX_FIELD_LENGTH) {
-            $errorsLength[] = 'Le nom du moniteur ne peut pas dépasser' . self::MAX_FIELD_LENGTH . ' caractères';
+            $errorsLength[] = 'Le nom du moniteur ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
         }
 
         if (strlen($activities['image']) > self::MAX_FIELD_LENGTH) {
-            $errorsLength[] = 'L\'url de l\' image ne peut pas dépasser' . self::MAX_FIELD_LENGTH . ' caractères';
+            $errorsLength[] = 'L\'url de l\' image ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
         }
 
-        if (strlen($activities['description']) < self::MAX_FIELD_LENGTH) {
-            $errorsLength[] = 'La description de l\'activité ne peut pas dépasser' . self::MAX_FIELD_LENGTH .
+        if (strlen($activities['description']) > self::MAX_FIELD_LENGTH) {
+            $errorsLength[] = 'La description de l\'activité ne peut pas dépasser ' . self::MAX_FIELD_LENGTH .
                 ' caractères';
         }
 
         if (strlen($activities['name']) < self::MIN_FIELD_LENGTH) {
-            $errorsLength[] = 'Le nom de l\'activité doit faire plus de' . self::MIN_FIELD_LENGTH . ' caractères';
+            $errorsLength[] = 'Le nom de l\'activité doit faire plus de ' . self::MIN_FIELD_LENGTH . ' caractères';
         }
 
         if (strlen($activities['instructor_name']) < self::MIN_FIELD_LENGTH) {
-            $errorsLength[] = 'Le nom du moniteur doit faire plus de' . self::MIN_FIELD_LENGTH . ' caractères';
+            $errorsLength[] = 'Le nom du moniteur doit faire plus de ' . self::MIN_FIELD_LENGTH . ' caractères';
         }
 
         return $errorsLength;
     }
-    
+
     public function index(): string
     {
         $activityManager = new ActivityManager();

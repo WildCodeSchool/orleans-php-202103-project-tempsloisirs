f11586481fa1d4c020310cbe3b3919a745eb7daf
eguesbaya <guesbaya@hotmail.fr>
2021-04-22 17:33:40 +0200

    test

--- src/Controller/AdminActivityController.php
+++ src/Controller/AdminActivityController.php
@@ -6,6 +6,10 @@ use App\Model\ActivityManager;
 
 class AdminActivityController extends AbstractController
 {
+    // delete
+    public const MAX_FIELD_LENGTH = 255;
+    public const MIN_FIELD_LENGTH = 2;
+
     public function index(): string
     {
         $activityManager = new ActivityManager();
@@ -13,24 +17,118 @@ class AdminActivityController extends AbstractController
         return $this->twig->render('Admin/Activity/index.html.twig', ['activities' => $activities]);
     }
 
-    public function edit(int $id): string
+    public function edit($id): string
     {
         $activityManager = new ActivityManager();
         $activities = $activityManager->selectOneById($id);
+        $errors = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
             $activities = array_map('trim', $_POST);
+            $activities = array_map('ucfirst', $activities);
 
             // TODO validations (length, format...)
+            $errorsEmpty = $this->validateEmpty($activities);
+            $errorsLength = $this->validateLength($activities);
+            $errorsURL = $this->validateURL($activities);
+            $errorsTime = $this->validateTime($activities);
+            $errors = array_merge($errorsEmpty, $errorsLength, $errorsURL, $errorsTime);
 
-            // if validation is ok, update and redirection
-            $activityManager->update($activities);
-            header('Location: /AdminActivity/show/' . $id);
+            if (empty($errors)) {
+                //$activityManager = new ActivityManager();
+                $id = $activities['id'];
+                $activityManager->update($activities);
+                header('Location: /Admin/Activity/index');
+            }
+        }
+        return $this->twig->render('Admin/Activity/edit.html.twig', [
+            'activities' => $activities, 'errors' => $errors,
+        ]);
+    }
+    // Delete everything past this before committing
+
+    public function validateEmpty(array $activities): array
+    {
+        $errorsEmpty = [];
+        if (empty($activities['name'])) {
+            $errorsEmpty[] = "Le nom de l'activité est obligatoire.";
         }
 
-        return $this->twig->render('Admin/Activity/edit.html.twig', [
-            'activities' => $activities,
-        ]);
+        if (empty($activities['weekday'])) {
+            $errorsEmpty[] = "Vous devez indiquer le jour.";
+        }
+
+        if (empty($activities['instructor_name'])) {
+            $errorsEmpty[] = "Le nom de l'animateur est obligatoire";
+        }
+
+        if (empty($activities['start_time'])) {
+            $errorsEmpty[] = "Veuillez indiquer une heure de début pour cette activité";
+        }
+
+        if (empty($activities['end_time'])) {
+            $errorsEmpty[] = "Veuillez indiquer une heure de début pour cette activité";
+        }
+
+        return $errorsEmpty;
+    }
+
+    //Delete everything past this before pushing
+
+    public function validateURL(array $activities): array
+    {
+        $errorsURL = [];
+
+        if (empty(!$activities['image'])) {
+            if (!filter_var($activities['image'], FILTER_VALIDATE_URL)) {
+                $errorsURL[] = "Cette URL n'est pas valide.";
+            }
+        }
+
+        return $errorsURL;
+    }
+
+    public function validateTime(array $activities): array
+    {
+        $errorsTime = [];
+
+        if ($activities['start_time'] > $activities['end_time']) {
+            $errorsTime[] = 'L\'heure de fin de l\'activité doit être postérieure à l\'heure de début.';
+        }
+
+        return $errorsTime;
+    }
+
+    public function validateLength(array $activities): array
+    {
+        $errorsLength = [];
+
+        if (strlen($activities['name']) > self::MAX_FIELD_LENGTH) {
+            $errorsLength[] = 'Le nom de l\'activité ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
+        }
+
+        if (strlen($activities['instructor_name']) > self::MAX_FIELD_LENGTH) {
+            $errorsLength[] = 'Le nom du moniteur ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
+        }
+
+        if (strlen($activities['image']) > self::MAX_FIELD_LENGTH) {
+            $errorsLength[] = 'L\'url de l\' image ne peut pas dépasser ' . self::MAX_FIELD_LENGTH . ' caractères';
+        }
+
+        if (strlen($activities['description']) > self::MAX_FIELD_LENGTH) {
+            $errorsLength[] = 'La description de l\'activité ne peut pas dépasser ' . self::MAX_FIELD_LENGTH .
+                ' caractères';
+        }
+
+        if (strlen($activities['name']) < self::MIN_FIELD_LENGTH) {
+            $errorsLength[] = 'Le nom de l\'activité doit faire plus de ' . self::MIN_FIELD_LENGTH . ' caractères';
+        }
+
+        if (strlen($activities['instructor_name']) < self::MIN_FIELD_LENGTH) {
+            $errorsLength[] = 'Le nom du moniteur doit faire plus de ' . self::MIN_FIELD_LENGTH . ' caractères';
+        }
+
+        return $errorsLength;
     }
 }
--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -11,9 +11,17 @@ class ActivityManager extends AbstractManager
      */
     public function update(array $activities): bool
     {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name WHERE id=:id");
-        $statement->bindValue('id', $activities['id'], \PDO::PARAM_INT);
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name`, `weekday`, `instructor_name`, 
+        `start_time`, `end_time`, `image`, `description` = :name, :weekday, :instructor_name, :start_time, :end_time,
+        :image, :description WHERE id=:id");
         $statement->bindValue('name', $activities['name'], \PDO::PARAM_STR);
+        $statement->bindValue('weekday', $activities['weekday'], \PDO::PARAM_STR);
+        $statement->bindValue('instructor_name', $activities['instructor_name'], \PDO::PARAM_STR);
+        $statement->bindValue('start_time', $activities['start_time'], \PDO::PARAM_STR);
+        $statement->bindValue('end_time', $activities['end_time'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $activities['image'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $activities['description'], \PDO::PARAM_STR);
+        $statement->bindValue('id', $activities['id'], \PDO::PARAM_INT);
 
         return $statement->execute();
     }

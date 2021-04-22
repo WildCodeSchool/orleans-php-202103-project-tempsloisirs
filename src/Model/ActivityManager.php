1f595176cae888e49e82160158c846eda798303d
eguesbaya <guesbaya@hotmail.fr>
2021-04-22 12:07:29 +0200

    Removed update function in ActivityManager

--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -23,16 +23,4 @@ class ActivityManager extends AbstractManager
         $statement->bindValue('description', $activity['description'], \PDO::PARAM_STR);
         return $statement->execute();
     }
-
-    /**
-     * Update item in database
-     */
-    public function update(array $item): bool
-    {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
 }

1fcc5c96d74d22392d7313295751c2ed5ba01977
eguesbaya <guesbaya@hotmail.fr>
2021-04-27 16:37:11 +0200

    Created loop for day filters. Started loop for activities by day

---
+++ src/Controller/ActivityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ActivityManager;
+
+class ActivityController extends AbstractController
+{
+    public const MAX_FIELD_LENGTH = 255;
+    public const MIN_FIELD_LENGTH = 2;
+    public const DAYS = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
+
+    public function index(): string
+    {
+        $activityManager = new ActivityManager();
+        $activities = $activityManager->selectAll('name');
+        return $this->twig->render('Home/activity.html.twig', ['activities' => $activities]);
+    }
+}

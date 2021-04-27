66ba4efa4543d8d3cf5663340774f3a7aca6569a
glochlainn <lochlainn.gadault@gmail.com>
2021-04-27 16:57:44 +0200

    init navbar

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class HomeController extends AbstractController
+{
+    public function index()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+}

e9f01139400dcd46b39ba8cecaa470a2673bd6f5
glochlainn <lochlainn.gadault@gmail.com>
2021-04-27 16:04:56 +0200

    minor fix

--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -5,4 +5,22 @@ namespace App\Model;
 class ActivityManager extends AbstractManager
 {
     public const TABLE = 'activity';
+
+    /**
+     * Insert new item in database
+     */
+    public function insert(array $activity)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
+        " (`name`,`weekday`, `instructor_name`, `start_time`, `end_time`, `image`, `description`) 
+        VALUES (:name, :weekday, :instructor_name, :start_time, :end_time, :image, :description)");
+        $statement->bindValue('name', $activity['name'], \PDO::PARAM_STR);
+        $statement->bindValue('weekday', $activity['weekday'], \PDO::PARAM_STR);
+        $statement->bindValue('instructor_name', $activity['instructor_name'], \PDO::PARAM_STR);
+        $statement->bindValue('start_time', $activity['start_time'], \PDO::PARAM_STR);
+        $statement->bindValue('end_time', $activity['end_time'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $activity['image'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $activity['description'], \PDO::PARAM_STR);
+        return $statement->execute();
+    }
 }

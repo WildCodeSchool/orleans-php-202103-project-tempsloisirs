399c0961cf055990e906c26ad9addec3b62d67c8
eguesbaya <guesbaya@hotmail.fr>
2021-04-29 11:30:01 +0200

    Added update method in ActivityManager

--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -24,8 +24,12 @@ class ActivityManager extends AbstractManager
         return $statement->execute();
     }
 
-    public function update()
+    public function update(array $activities): bool
     {
-        return "Test";
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name WHERE id=:id");
+        $statement->bindValue('id', $activities['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $activities['name'], \PDO::PARAM_STR);
+
+        return $statement->execute();
     }
 }

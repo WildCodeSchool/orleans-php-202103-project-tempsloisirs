f0ceabf5a6c1149f303a87ebe87b8cb40e0653c9
eguesbaya <guesbaya@hotmail.fr>
2021-05-01 19:40:15 +0200

    committing again

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-//use App\Model\ActivityManager;
+use App\Model\HomeManager;
 
 class HomeController extends AbstractController
 {
     public function index(): string
     {
         $homeManager = new HomeManager();
-        $information = $homeManager->selectAll('date');
+        $information = $homeManager->selectInfo('date');
         return $this->twig->render('Home/index.html.twig', ['information' => $information]);
     }
 }

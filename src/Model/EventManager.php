598bad05972d43fff127ebb0a67ea0d39b96486f
eguesbaya <guesbaya@hotmail.fr>
2021-05-03 22:39:29 +0200

    Replaced code with loops

--- src/Model/EventManager.php
+++ src/Model/EventManager.php
@@ -5,6 +5,18 @@ namespace App\Model;
 class EventManager extends AbstractManager
 {
     public const TABLE = 'event';
+    public const MONTHS = [ 1=>'Janvier',
+        2 =>'Février',
+        3=>'Mars',
+        4=>'Avril',
+        5=>'Mai',
+        6=>'Juin',
+        7=>'Juillet',
+        8=>'Aout',
+        9=>'Septembre',
+        10=>'Octobre',
+        11=>'Novembre',
+        12=>'Décembre'];
 
     public function updateEvent(array $event)
     {
@@ -37,4 +49,5 @@ class EventManager extends AbstractManager
 
         return $statement->execute();
     }
+
 }

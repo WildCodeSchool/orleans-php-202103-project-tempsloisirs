e92a63ae5b24da586335d93205a4fab76fa82ca8
Gersey Stelmach <gerseystelmach@gmail.com>
2021-05-04 13:57:12 +0200

    first part of home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ActivityManager;
+use App\Model\EventManager;
+use App\Model\InformationManager;
+
+class HomeController extends AbstractController
+{
+    public function index(): string
+    {
+        $activityManager = new ActivityManager();
+        $eventManager = new EventManager();
+        $informationManager = new InformationManager();
+        $events = $eventManager->selectAll();
+        $activities = $activityManager->selectAll();
+        $informations = $informationManager->selectAll();
+        return $this->twig->render('Home/index.html.twig', ['events' => $events,
+        'activities' => $activities,
+        'informations' => $informations,]);
+    }
+}
